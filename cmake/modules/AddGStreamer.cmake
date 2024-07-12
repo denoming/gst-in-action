8fc3b7ff16fbaea8930f96932f88c545fb2086d3
Denys Asauliak <denoming@gmail.com>
2024-07-12 22:47:34 +0300

    add new gstreamer modules

--- cmake/modules/AddGStreamer.cmake
+++ cmake/modules/AddGStreamer.cmake
@@ -2,5 +2,7 @@ find_package(PkgConfig)
 
 pkg_check_modules(GStreamer REQUIRED IMPORTED_TARGET gstreamer-1.0)
 pkg_check_modules(GStreamerBase REQUIRED IMPORTED_TARGET gstreamer-base-1.0)
+pkg_check_modules(GStreamerAudio REQUIRED IMPORTED_TARGET gstreamer-audio-1.0)
+pkg_check_modules(GStreamerPbUtils REQUIRED IMPORTED_TARGET gstreamer-pbutils-1.0)
 pkg_check_modules(GStreamerPluginsBase REQUIRED IMPORTED_TARGET gstreamer-plugins-base-1.0)
 pkg_check_modules(GStreamerPluginsBad REQUIRED IMPORTED_TARGET gstreamer-plugins-bad-1.0)
